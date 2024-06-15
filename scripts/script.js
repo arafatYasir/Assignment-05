1e5d45727ee7bbb712c79d64006de6863f080b49
arafatYasir <arafat.yasir1305@gmail.com>
2024-06-15 11:06:50 +0600

    Full Design Complete

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,15 @@
-console.log("Hello World!");
+var totalPrice = 0;
+var n = 1;
+
+function add(name, price)
+{
+    const product = document.createElement("h3");
+    product.innerText = n + ". " + name;
+    const cart = document.getElementById("cart-section");
+    cart.appendChild(product);
+    cart.style.marginBottom = "16px";
+
+    totalPrice += price;
+    document.getElementById("total-price").innerText = totalPrice + " TK";
+    n++;
+}
